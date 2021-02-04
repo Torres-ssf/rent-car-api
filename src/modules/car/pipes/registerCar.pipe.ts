7515b88df46440378473cf71a7ae5eb6ceb87c4e
Torres-ssf <sergio.uft@gmail.com>
2021-02-04 19:08:02 -0300

    feat(register car pipe): implement register car pipe to validate body request

---
+++ src/modules/car/pipes/registerCar.pipe.ts
@@ -0,0 +1,21 @@
+import { AppError } from '@shared/errors/AppError';
+import { extractValidationErrors } from '@shared/utils/formatValidationError';
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
+import { RegisterCarDTO } from '../useCases/RegisterCar/RegisterCarDTO';
+
+export const createShortsterPipe = async (
+  data: RegisterCarDTO,
+): Promise<RegisterCarDTO> => {
+  const registerCarDTO = plainToClass(RegisterCarDTO, data);
+
+  const paramErrors = await validate(registerCarDTO);
+
+  if (paramErrors.length) {
+    const errorMessages = extractValidationErrors(paramErrors);
+
+    throw new AppError(errorMessages);
+  }
+
+  return registerCarDTO;
+};
