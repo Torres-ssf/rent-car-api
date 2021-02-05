74abf9d46e1e5582a04df8df747c8017e9b5bbbc
Torres-ssf <sergio.uft@gmail.com>
2021-02-04 22:25:56 -0300

    test(register car endpoint): ensure name and brand have no empty spaces at start/end

--- src/modules/car/routes/car.routes.spec.ts
+++ src/modules/car/routes/car.routes.spec.ts
@@ -1,10 +1,10 @@
 import 'reflect-metadata';
 
 import request from 'supertest';
-import { app } from '../../../shared/app';
+import { app } from '@shared/app';
 
 describe('Car Register Endpoint', () => {
-  it('ensure that car name, brand, and dailyValue are present', async () => {
+  it('ensures that name, brand, and daily_value are present', async () => {
     const res = await request(app).post('/car').send({});
 
     expect(res.status).toBe(400);
@@ -14,7 +14,7 @@ describe('Car Register Endpoint', () => {
     expect(res.body.message).toContain('daily_value must be a positive number');
   });
 
-  it('ensure that car name and brand have at least 3 chars length', async () => {
+  it('ensures that name and brand have at least 3 chars length', async () => {
     const res = await request(app).post('/car').send({
       name: 'as',
       brand: 'sa',
@@ -31,7 +31,7 @@ describe('Car Register Endpoint', () => {
     );
   });
 
-  it('ensure that car name and brand length are not higher than 80 chars', async () => {
+  it('ensures that name and brand length are not higher than 80 chars', async () => {
     const res = await request(app).post('/car').send({
       name:
         'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially un',
@@ -50,7 +50,7 @@ describe('Car Register Endpoint', () => {
     );
   });
 
-  it('ensure that daily_value is a positive value', async () => {
+  it('ensures that daily_value is a positive value', async () => {
     const res = await request(app).post('/car').send({
       name: 'V8',
       brand: 'Ferrari',
@@ -60,5 +60,17 @@ describe('Car Register Endpoint', () => {
     expect(res.status).toBe(400);
     expect(res.body).toHaveProperty('message');
     expect(res.body.message).toContain('daily_value must be a positive number');
+  });
+
+  it('ensures that name and brand have no empty spaces at the start/end', async () => {
+    const res = await request(app).post('/car').send({
+      name: '          Enzo ',
+      brand: '  Ferrari    ',
+      daily_value: 900,
+    });
+
+    expect(res.status).toBe(200);
+    expect(res.body.name === 'Enzo').toBeTruthy();
+    expect(res.body.brand === 'Ferrari').toBeTruthy();
   });
 });
