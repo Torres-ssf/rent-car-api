9623ff3516146ca95a2c78053c4ba082f29fbe8d
Torres-ssf <sergio.uft@gmail.com>
2021-02-04 22:08:00 -0300

    test(register car endpoint): ensure car name and brand are at least 3 chars length

--- src/modules/car/routes/car.routes.spec.ts
+++ src/modules/car/routes/car.routes.spec.ts
@@ -13,4 +13,23 @@ describe('Car Register Endpoint', () => {
     expect(res.body.message).toContain('brand must be a string');
     expect(res.body.message).toContain('daily_value must be a positive number');
   });
+
+  it('should ensure that car name and brand are not empty string', async () => {
+    const res = await request(app).post('/car').send({
+      name: 'as',
+      brand: 'sa',
+      dailyValue: 900,
+    });
+
+    console.log(res);
+
+    expect(res.status).toBe(400);
+    expect(res.body).toHaveProperty('message');
+    expect(res.body.message).toContain(
+      'name must be longer than or equal to 3 characters',
+    );
+    expect(res.body.message).toContain(
+      'brand must be longer than or equal to 3 characters',
+    );
+  });
 });
