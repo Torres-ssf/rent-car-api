948cdc12db8612d217b3dfe9bde0f266f1021c75
Torres-ssf <sergio.uft@gmail.com>
2021-02-04 21:13:54 -0300

    feat(register car dto): update RegisterCarDTO to trim name and car brand

--- src/modules/car/useCases/RegisterCar/RegisterCarDTO.ts
+++ src/modules/car/useCases/RegisterCar/RegisterCarDTO.ts
@@ -1,14 +1,26 @@
-import { IsNumber, IsString, MaxLength } from 'class-validator';
+import { Transform } from 'class-transformer';
+import {
+  IsNumber,
+  IsString,
+  MaxLength,
+  IsPositive,
+  MinLength,
+} from 'class-validator';
 
 export class RegisterCarDTO {
   @IsString()
+  @MinLength(3)
   @MaxLength(80)
+  @Transform(({ value }) => value.trim())
   name: string;
 
   @IsString()
   @MaxLength(80)
+  @MinLength(3)
+  @Transform(({ value }) => value.trim())
   brand: string;
 
+  @IsPositive()
   @IsNumber()
   dailyValue: number;
 }
