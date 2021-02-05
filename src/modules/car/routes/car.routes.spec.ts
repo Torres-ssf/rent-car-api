9abdb0312f67db67dd57e373b33c69fdc0561389
Torres-ssf <sergio.uft@gmail.com>
2021-02-04 22:15:03 -0300

    test(register car endpoint): ensure name and brand length are not longer than 80 chars

--- src/modules/car/routes/car.routes.spec.ts
+++ src/modules/car/routes/car.routes.spec.ts
@@ -4,7 +4,7 @@ import request from 'supertest';
 import { app } from '../../../shared/app';
 
 describe('Car Register Endpoint', () => {
-  it('should ensure that car name, brand, and dailyValue are present', async () => {
+  it('ensure that car name, brand, and dailyValue are present', async () => {
     const res = await request(app).post('/car').send({});
 
     expect(res.status).toBe(400);
@@ -14,14 +14,12 @@ describe('Car Register Endpoint', () => {
     expect(res.body.message).toContain('daily_value must be a positive number');
   });
 
-  it('should ensure that car name and brand are not empty string', async () => {
+  it('ensure that car name and brand have at least 3 chars length', async () => {
     const res = await request(app).post('/car').send({
       name: 'as',
       brand: 'sa',
-      dailyValue: 900,
+      daily_value: 900,
     });
-
-    console.log(res);
 
     expect(res.status).toBe(400);
     expect(res.body).toHaveProperty('message');
@@ -30,6 +28,25 @@ describe('Car Register Endpoint', () => {
     );
     expect(res.body.message).toContain(
       'brand must be longer than or equal to 3 characters',
+    );
+  });
+
+  it('ensure that car name and brand length are not higher than 80 chars', async () => {
+    const res = await request(app).post('/car').send({
+      name:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially un',
+      brand:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially un',
+      daily_value: 900,
+    });
+
+    expect(res.status).toBe(400);
+    expect(res.body).toHaveProperty('message');
+    expect(res.body.message).toContain(
+      'name must be shorter than or equal to 80 characters',
+    );
+    expect(res.body.message).toContain(
+      'name must be shorter than or equal to 80 characters',
     );
   });
 });
