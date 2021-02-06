e043e1b868740481ea8a21325bb027b2f56024af
Torres-ssf <sergio.uft@gmail.com>
2021-02-06 15:16:01 -0300

    test(register car endpoint): ensure model and brand have min 2 and max 80 char length

--- src/modules/car/routes/car.routes.spec.ts
+++ src/modules/car/routes/car.routes.spec.ts
@@ -4,33 +4,14 @@ import request from 'supertest';
 import { app } from '@shared/app';
 
 describe('Car Register Endpoint', () => {
-  it('ensures that name, brand, and daily_value are present', async () => {
-    const res = await request(app).post('/car').send({});
-
-    expect(res.status).toBe(400);
-    expect(res.body.message).toContain('name must be a string');
-    expect(res.body.message).toContain('brand must be a string');
-    expect(res.body.message).toContain('daily_value must be a positive number');
-  });
-
-  it('ensures that name and brand have at least 3 chars length', async () => {
-    const res = await request(app).post('/car').send({
+  it('ensures that name and brand have at least 2 chars length and max 80 chars length', async () => {
+    const resMinLength = await request(app).post('/car').send({
       name: 'as',
       brand: 'sa',
       daily_value: 900,
     });
 
-    expect(res.status).toBe(400);
-    expect(res.body.message).toContain(
-      'name must be longer than or equal to 3 characters',
-    );
-    expect(res.body.message).toContain(
-      'brand must be longer than or equal to 3 characters',
-    );
-  });
-
-  it('ensures that name and brand length are not higher than 80 chars', async () => {
-    const res = await request(app).post('/car').send({
+    const resMaxLength = await request(app).post('/car').send({
       name:
         'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially un',
       brand:
@@ -38,11 +19,19 @@ describe('Car Register Endpoint', () => {
       daily_value: 900,
     });
 
-    expect(res.status).toBe(400);
-    expect(res.body.message).toContain(
+    expect(resMinLength.status).toBe(400);
+    expect(resMinLength.body.message).toContain(
+      'name must be longer than or equal to 2 characters',
+    );
+    expect(resMinLength.body.message).toContain(
+      'brand must be longer than or equal to 2 characters',
+    );
+
+    expect(resMaxLength.status).toBe(400);
+    expect(resMaxLength.body.message).toContain(
       'name must be shorter than or equal to 80 characters',
     );
-    expect(res.body.message).toContain(
+    expect(resMaxLength.body.message).toContain(
       'name must be shorter than or equal to 80 characters',
     );
   });
