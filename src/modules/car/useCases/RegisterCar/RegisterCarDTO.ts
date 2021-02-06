3e67f975b05e7f399ebac54e69fe118a89160b07
Torres-ssf <sergio.uft@gmail.com>
2021-02-06 12:09:13 -0300

    refactor(register car dto): update DTO adding new car specifications

--- src/modules/car/useCases/RegisterCar/RegisterCarDTO.ts
+++ src/modules/car/useCases/RegisterCar/RegisterCarDTO.ts
@@ -5,20 +5,52 @@ import {
   MaxLength,
   IsPositive,
   MinLength,
+  IsEnum,
+  Min,
+  Max,
 } from 'class-validator';
+import { Transmission, Engine } from '../../models/enums';
 
 export class RegisterCarDTO {
   @IsString()
-  @MinLength(3)
+  @MinLength(2)
   @MaxLength(80)
   @Transform(({ value }) => value.trim())
-  name: string;
+  model: string;
 
   @IsString()
   @MaxLength(80)
-  @MinLength(3)
+  @MinLength(2)
   @Transform(({ value }) => value.trim())
   brand: string;
+
+  @IsPositive()
+  @IsNumber()
+  max_speed: number;
+
+  @IsPositive()
+  @IsNumber()
+  horse_power: number;
+
+  @IsPositive()
+  @IsNumber()
+  zero_to_one_hundred: number;
+
+  @IsString()
+  @IsEnum(Engine, { message: `engine must be one of ${Object.keys(Engine)}` })
+  @Transform(({ value }) => value.trim().toUpperCase())
+  engine: Engine;
+
+  @IsString()
+  @IsEnum(Transmission, {
+    message: `engine must be one of ${Object.keys(Transmission)}`,
+  })
+  @Transform(({ value }) => value.trim().toUpperCase())
+  transmission: Transmission;
+
+  @Min(2)
+  @Max(9)
+  passengers: number;
 
   @IsPositive()
   @IsNumber()
