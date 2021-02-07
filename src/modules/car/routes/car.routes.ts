8d4722a04d0f907dd9ffce64ea544174e0bb859a
Torres-ssf <sergio.uft@gmail.com>
2021-02-06 22:23:47 -0300

    feat(car routes): implement car update route

--- src/modules/car/routes/car.routes.ts
+++ src/modules/car/routes/car.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import { RegisterCarController } from '../useCases/RegisterCar/RegisterCarController';
+import { UpdateCarController } from '../useCases/UpdateCar/UpdateCarController';
 
 const registerCarController = new RegisterCarController();
+const updateCarController = new UpdateCarController();
 
 export const carRoutes = Router();
 
 carRoutes.post('/', registerCarController.handle);
+
+carRoutes.put('/:id', updateCarController.handle);
