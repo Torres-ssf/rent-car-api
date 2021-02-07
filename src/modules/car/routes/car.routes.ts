e713b533d87b8aba908e4cf048c48571e12f95c4
Torres-ssf <sergio.uft@gmail.com>
2021-02-07 18:03:03 -0300

    feat(list car endpoint): add list car route to the car routes file

--- src/modules/car/routes/car.routes.ts
+++ src/modules/car/routes/car.routes.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
+import { ListCarController } from '../useCases/ListCar/ListCarController';
 import { RegisterCarController } from '../useCases/RegisterCar/RegisterCarController';
 import { RemoveCarController } from '../useCases/RemoveCar/RemoveCarController';
 import { UpdateCarController } from '../useCases/UpdateCar/UpdateCarController';
 
+const listCarController = new ListCarController();
 const registerCarController = new RegisterCarController();
 const updateCarController = new UpdateCarController();
 const removeCarController = new RemoveCarController();
 
 export const carRoutes = Router();
+
+carRoutes.get('/', listCarController.handle);
 
 carRoutes.post('/', registerCarController.handle);
 
