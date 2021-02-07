f153e4e5e97c5bc8d57ad33335e0370e0ec946de
Torres-ssf <sergio.uft@gmail.com>
2021-02-07 17:25:30 -0300

    feat(car repository): add list method to ICarRepository interface

--- src/modules/car/repositories/ICarRepository.ts
+++ src/modules/car/repositories/ICarRepository.ts
@@ -2,6 +2,7 @@ import { Car } from '../models/Car';
 
 export interface ICarRepository {
   findById(id: string): Promise<Car | undefined>;
+  list(): Promise<Car[]>;
   save(car: Car): Promise<Car>;
   remove(id: string): Promise<void>;
 }
