b9bb355dc6b23568896e91b6c5d572467057e1f0
Torres-ssf <sergio.uft@gmail.com>
2021-02-07 16:02:21 -0300

    feat(remove car): immplement RemoveCarController

---
+++ src/modules/car/useCases/RemoveCar/RemoveCarController.ts
@@ -0,0 +1,22 @@
+import { AppError } from '@shared/errors/AppError';
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import { RemoveCarUseCase } from './RemoveCarUseCase';
+
+export class RemoveCarController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    try {
+      const { id } = request.params;
+
+      const removeCarUseCase = container.resolve(RemoveCarUseCase);
+
+      await removeCarUseCase.execute(id);
+
+      return response.send('hi');
+    } catch (err) {
+      throw new AppError(
+        err.message || 'error occurred while trying to delete car registry',
+      );
+    }
+  }
+}
