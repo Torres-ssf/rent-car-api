703b414e7466f863017686eb5fd3767ed79f39f8
Torres-ssf <sergio.uft@gmail.com>
2021-02-06 22:19:50 -0300

    test(update car): ensure all given properties are properlyy updated

--- src/modules/car/useCases/UpdateCar/UpdateCarUseCase.spec.ts
+++ src/modules/car/useCases/UpdateCar/UpdateCarUseCase.spec.ts
@@ -3,7 +3,6 @@ import 'reflect-metadata';
 import { Engine, Transmission } from '@modules/car/models/enums';
 import { FakeCarRepository } from '@modules/car/repositories/fakes/FakeCarRepository';
 import { Car } from '@modules/car/models/Car';
-import { AppError } from '@shared/errors/AppError';
 import { UpdateCarUseCase } from './UpdateCarUseCase';
 
 describe('UpdateCarUseCase', () => {
@@ -11,7 +10,7 @@ describe('UpdateCarUseCase', () => {
 
   let carRepository: FakeCarRepository;
 
-  const carParams = {
+  const carParams1 = {
     model: 'F8',
     brand: 'Ferrari',
     max_speed: 340,
@@ -21,6 +20,18 @@ describe('UpdateCarUseCase', () => {
     transmission: Transmission.Automatic,
     passengers: 2,
     daily_value: 900,
+  };
+
+  const carParams2 = {
+    model: 'Aventador',
+    brand: 'Lamborghini',
+    max_speed: 350,
+    horse_power: 770,
+    zero_to_one_hundred: 2.8,
+    engine: Engine.Gas,
+    transmission: Transmission.Automatic,
+    passengers: 2,
+    daily_value: 1100,
   };
 
   beforeEach(() => {
@@ -35,5 +46,15 @@ describe('UpdateCarUseCase', () => {
         model: 'New Beatle',
       }),
     ).rejects.toHaveProperty('message', 'no car found for the given id');
+  });
+
+  it('ensures all given properties are properly updated', async () => {
+    const newCar = await carRepository.save(
+      Object.assign(new Car(), carParams1),
+    );
+
+    await expect(
+      updateCarUseCase.execute(newCar.id, carParams2),
+    ).resolves.toMatchObject(carParams2);
   });
 });
