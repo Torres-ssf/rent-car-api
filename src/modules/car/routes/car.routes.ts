cd32700536ed26e664df634fdcc8de22e148d1b2
Torres-ssf <sergio.uft@gmail.com>
2021-02-07 16:02:44 -0300

    feat(car routes): add remove car route to car routes file

--- src/modules/car/routes/car.routes.ts
+++ src/modules/car/routes/car.routes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 import { RegisterCarController } from '../useCases/RegisterCar/RegisterCarController';
+import { RemoveCarController } from '../useCases/RemoveCar/RemoveCarController';
 import { UpdateCarController } from '../useCases/UpdateCar/UpdateCarController';
 
 const registerCarController = new RegisterCarController();
 const updateCarController = new UpdateCarController();
+const removeCarController = new RemoveCarController();
 
 export const carRoutes = Router();
 
 carRoutes.post('/', registerCarController.handle);
 
 carRoutes.put('/:id', updateCarController.handle);
+
+carRoutes.delete('/:id', removeCarController.handle);
