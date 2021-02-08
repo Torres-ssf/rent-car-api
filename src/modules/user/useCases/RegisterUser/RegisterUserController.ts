2e163f75b3c99e3a8989f67be8d0c0f2eab0b97a
Torres-ssf <sergio.uft@gmail.com>
2021-02-08 10:48:17 -0300

    feat(register user controller): ensures returned user object has no password property

--- src/modules/user/useCases/RegisterUser/RegisterUserController.ts
+++ src/modules/user/useCases/RegisterUser/RegisterUserController.ts
@@ -1,3 +1,4 @@
+import { AppError } from '@shared/errors/AppError';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import { registerUserPipe } from '../../pipes/registerUser.pipe';
@@ -5,12 +6,22 @@ import { RegisterUserUseCase } from './RegisterUserUseCase';
 
 export class RegisterUserController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { name, email, password } = await registerUserPipe(request.body);
+    try {
+      const { name, email, password } = await registerUserPipe(request.body);
 
-    const registerUserUseCase = container.resolve(RegisterUserUseCase);
+      const registerUserUseCase = container.resolve(RegisterUserUseCase);
 
-    const user = await registerUserUseCase.execute({ name, email, password });
+      const { password: removed, ...user } = await registerUserUseCase.execute({
+        name,
+        email,
+        password,
+      });
 
-    return response.json(user);
+      return response.json(user);
+    } catch (err) {
+      throw new AppError(
+        err.message || 'error occurred while trying to register new user',
+      );
+    }
   }
 }
