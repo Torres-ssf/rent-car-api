289c7ff4bc25f5a3ecdffaf05a3de5d8591011f8
Torres-ssf <sergio.uft@gmail.com>
2021-02-09 14:32:49 -0300

    feat(create session): implement CreateSessionUseCase

---
+++ src/modules/user/useCases/CreateSession/CreateSessionUseCase.ts
@@ -0,0 +1,25 @@
+import { inject, injectable } from 'tsyringe';
+import { AppError } from '@shared/errors/AppError';
+import { IHashProvider } from '../../providers/HashProvider/models/IHashProvider';
+import { IUserRepository } from '../../repositories/IUserRepository';
+import { CreateSessionDTO } from './CreateSessionDTO';
+
+@injectable()
+export class CreateSessionUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUserRepository,
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  async execute(createSession: CreateSessionDTO): Promise<void> {
+    const { email, password } = createSession;
+
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('wrong email/password combination', 403);
+    }
+  }
+}
