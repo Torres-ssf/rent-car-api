0e04ecd1a05e928e3b139d9308d1c9337370e2af
Torres-ssf <sergio.uft@gmail.com>
2021-02-10 14:07:57 -0300

    test(create session endpoint): verify if password match password constraints

--- src/modules/user/routes/session.routes.spec.ts
+++ src/modules/user/routes/session.routes.spec.ts
@@ -49,4 +49,46 @@ describe('Create Session Endpoint', () => {
       'password must be shorter than or equal to 20 characters',
     );
   });
+
+  it('should verify if password has at least 1 number, 1 lower and 1 upper case letters', async () => {
+    const res1 = await request(app).post('/session/signin').send({
+      email: userParams.email,
+      password: 'asASfdsf',
+    });
+
+    const res2 = await request(app).post('/session/signin').send({
+      email: userParams.email,
+      password: 'asas1212',
+    });
+
+    const res3 = await request(app).post('/session/signin').send({
+      email: userParams.email,
+      password: 'ASAS1212',
+    });
+
+    const res4 = await request(app).post('/session/signin').send({
+      email: userParams.email,
+      password: 'asAS1212 ',
+    });
+
+    expect(res1.status).toBe(400);
+    expect(res1.body.message).toContain(
+      'Password should have at least one number, one lower letter, one upper letter and no empty spaces',
+    );
+
+    expect(res2.status).toBe(400);
+    expect(res2.body.message).toContain(
+      'Password should have at least one number, one lower letter, one upper letter and no empty spaces',
+    );
+
+    expect(res3.status).toBe(400);
+    expect(res3.body.message).toContain(
+      'Password should have at least one number, one lower letter, one upper letter and no empty spaces',
+    );
+
+    expect(res4.status).toBe(400);
+    expect(res4.body.message).toContain(
+      'Password should have at least one number, one lower letter, one upper letter and no empty spaces',
+    );
+  });
 });
