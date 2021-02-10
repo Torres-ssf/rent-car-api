2a8cddd8b1dd60b418f5b7f6e922975da4e924ba
Torres-ssf <sergio.uft@gmail.com>
2021-02-10 14:00:40 -0300

    test(create session endpoint): ensure password has at least 8 or max 20 chars

--- src/modules/user/routes/session.routes.spec.ts
+++ src/modules/user/routes/session.routes.spec.ts
@@ -10,13 +10,13 @@ describe('Create Session Endpoint', () => {
     password: 'fdsSs932',
   };
 
-  it('should check of email has email syntax', async () => {
-    const res1 = await request(app).post('/user').send({
+  it('should check if email has email syntax', async () => {
+    const res1 = await request(app).post('/session/signin').send({
       email: 'paul',
       password: userParams.password,
     });
 
-    const res2 = await request(app).post('/user').send({
+    const res2 = await request(app).post('/session/signin').send({
       email: 'paul@email',
       password: userParams.password,
     });
@@ -26,5 +26,27 @@ describe('Create Session Endpoint', () => {
 
     expect(res2.status).toBe(400);
     expect(res2.body.message).toContain('email must be an email');
+  });
+
+  it('should check if password has at least 8 chars long and 20 chars max', async () => {
+    const res1 = await request(app).post('/session/signin').send({
+      email: userParams.email,
+      password: '1234567',
+    });
+
+    const res2 = await request(app).post('/session/signin').send({
+      email: userParams.email,
+      password: '012345678901234567890',
+    });
+
+    expect(res1.status).toBe(400);
+    expect(res1.body.message).toContain(
+      'password must be longer than or equal to 8 characters',
+    );
+
+    expect(res2.status).toBe(400);
+    expect(res2.body.message).toContain(
+      'password must be shorter than or equal to 20 characters',
+    );
   });
 });
