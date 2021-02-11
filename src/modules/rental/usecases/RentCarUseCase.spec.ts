68a7a19c4ce1df151915bcde92f07a3708af1545
Torres-ssf <sergio.uft@gmail.com>
2021-02-11 12:36:06 -0300

    test(rent car): ensure end_date is does not come before start_date

--- src/modules/rental/usecases/RentCarUseCase.spec.ts
+++ src/modules/rental/usecases/RentCarUseCase.spec.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import MockDate from 'mockdate';
 
 import { FakeCarRepository } from '@modules/car/repositories/fakes/FakeCarRepository';
 import { RentCarUseCase } from './RentCarUseCase';
@@ -24,9 +25,25 @@ describe('ListCarUseCase', () => {
       rentCarUseCase.execute({
         car_id: '1',
         client_id: '1',
-        start_date: new Date(1989, 6, 10),
-        end_date: new Date(1987, 1, 11),
+        start_date: new Date(1987, 1, 11),
+        end_date: new Date(1989, 6, 10),
       }),
-    ).rejects.toHaveProperty('message', "you can't rent a car for a past date");
+    ).rejects.toHaveProperty('message', "you can't rent a car on a past date");
+  });
+
+  it('should not be possible for the end date happens before the starting date', async () => {
+    MockDate.set('2021-01-10');
+
+    await expect(
+      rentCarUseCase.execute({
+        car_id: '1',
+        client_id: '1',
+        start_date: new Date(2021, 1, 26),
+        end_date: new Date(2021, 1, 12),
+      }),
+    ).rejects.toHaveProperty(
+      'message',
+      "the end date can't be before the starting date",
+    );
   });
 });
