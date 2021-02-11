ad465d5825990fa73a19068ac828f57f5fce3c23
Torres-ssf <sergio.uft@gmail.com>
2021-02-11 14:33:44 -0300

    test(rent car): verify if user exists for the given user id

--- src/modules/rental/usecases/RentCarUseCase.spec.ts
+++ src/modules/rental/usecases/RentCarUseCase.spec.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 import MockDate from 'mockdate';
 
 import { FakeCarRepository } from '@modules/car/repositories/fakes/FakeCarRepository';
-import { isSameDay } from 'date-fns';
+import { FakeUserRepository } from '@modules/user/repositories/fakes/FakeUserRepository';
 import { RentCarUseCase } from './RentCarUseCase';
 import { FakeRentalRepository } from '../repositories/fakes/FakeRentalRepository';
 
@@ -13,12 +13,20 @@ describe('ListCarUseCase', () => {
 
   let carRepository: FakeCarRepository;
 
+  let userRepository: FakeUserRepository;
+
   beforeEach(() => {
     carRepository = new FakeCarRepository();
 
     rentalRepository = new FakeRentalRepository();
 
-    rentCarUseCase = new RentCarUseCase(carRepository, rentalRepository);
+    userRepository = new FakeUserRepository();
+
+    rentCarUseCase = new RentCarUseCase(
+      userRepository,
+      carRepository,
+      rentalRepository,
+    );
   });
 
   it('should not be possible to rent a car in a past date', async () => {
@@ -33,7 +41,6 @@ describe('ListCarUseCase', () => {
   });
 
   it('should not be possible for the end date happens before the starting date', async () => {
-    // MockDate.set('2021-01-10');
     global.Date.now = jest.fn(() => new Date(2021, 1, 10).getTime());
 
     await expect(
@@ -52,7 +59,6 @@ describe('ListCarUseCase', () => {
   });
 
   it('should not be possible for start date and end date be on the same day', async () => {
-    // MockDate.set('2021-01-10');
     global.Date.now = jest.fn(() => new Date(2021, 1, 10).getTime());
 
     await expect(
@@ -84,5 +90,18 @@ describe('ListCarUseCase', () => {
       'message',
       'rent store is close right now, try schedule a rent for tomorrow',
     );
+  });
+
+  it('should verify if user exists for the given id', async () => {
+    global.Date.now = jest.fn(() => new Date(2021, 1, 11).getTime());
+
+    await expect(
+      rentCarUseCase.execute({
+        car_id: '1',
+        client_id: 'nonExistentUser',
+        start_date: new Date(2021, 1, 12),
+        end_date: new Date(2021, 1, 14),
+      }),
+    ).rejects.toHaveProperty('message', 'no user was found for the given id');
   });
 });
