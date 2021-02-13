da363c6eec50f7f7ad5fc2e60907ff373ff3b083
Torres-ssf <sergio.uft@gmail.com>
2021-02-13 15:55:06 -0300

    feat(rentcar pipe): implement rentCar pipe to validate rentCar data

---
+++ src/modules/rental/pipes/rentCar.pipe.ts
@@ -0,0 +1,19 @@
+import { AppError } from '@shared/errors/AppError';
+import { extractValidationErrors } from '@shared/utils/formatValidationError';
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
+import { RentCarDTO } from '../useCases/RentCarDTO';
+
+export const rentCarPipe = async (data: RentCarDTO): Promise<RentCarDTO> => {
+  const rentCarDTO = plainToClass(RentCarDTO, data);
+
+  const paramErrors = await validate(rentCarDTO);
+
+  if (paramErrors.length) {
+    const errorMessages = extractValidationErrors(paramErrors);
+
+    throw new AppError(errorMessages);
+  }
+
+  return rentCarDTO;
+};
