d0000eecf50af2670ecd6b010c1b850199de7494
Torres-ssf <sergio.uft@gmail.com>
2021-02-13 08:11:35 -0300

    feat(rental repository): implement findByCarIdAndDate at FakeRentalRepository

--- src/modules/rental/repositories/fakes/FakeRentalRepository.ts
+++ src/modules/rental/repositories/fakes/FakeRentalRepository.ts
@@ -1,16 +1,25 @@
+import { IFindByCarAndDate } from '@modules/rental/dtos/IFindByCarAndDate';
 import { Rental } from '@modules/rental/models/Rental';
 import { IRentalRepository } from '../IRentalRepository';
 
 export class FakeRentalRepository implements IRentalRepository {
-  private rentals: Rental[] = [];
+  rentals: Rental[] = [];
 
   async save(rental: Rental): Promise<Rental> {
     this.rentals.push(rental);
     return rental;
   }
 
-  findByCarId(carId: string): Promise<Rental> {
-    throw new Error('Method not implemented.');
+  async findByCarIdAndDate(data: IFindByCarAndDate): Promise<Rental[]> {
+    const { car_id, start_date, end_date } = data;
+
+    return this.rentals.filter(rental => {
+      return (
+        rental.car_id === car_id &&
+        ((rental.start_date <= start_date && rental.end_date >= start_date) ||
+          (rental.start_date >= start_date && rental.start_date <= end_date))
+      );
+    });
   }
 
   findByUserId(userId: string): Promise<Rental> {
