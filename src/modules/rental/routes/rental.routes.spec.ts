9fe32a6810f9c8db637321546762a98d364be5ea
Torres-ssf <sergio.uft@gmail.com>
2021-02-13 16:35:45 -0300

    test(rent car endpoint): ensure start_date and end_date are date strings

--- src/modules/rental/routes/rental.routes.spec.ts
+++ src/modules/rental/routes/rental.routes.spec.ts
@@ -29,4 +29,33 @@ describe('Rent Car Endpoint', () => {
       expect(res.body.message).not.toContain('client_id must be a UUID');
     });
   });
+  describe('start_date and end_date params must be date strings', () => {
+    it('should return related error message when params are not string dates', async () => {
+      const res = await request(app).post('/rental').send({
+        car_id: '409b03e3-e6ee-4a84-96ea-1bb7cc329091',
+        client_id: '2645a491-2b5d-480f-887d-7ac647903b99',
+        start_date: 'not date string',
+        end_date: 'not date string',
+      });
+
+      expect(res.body.message).toContain('start_date must be a Date instance');
+      expect(res.body.message).toContain('end_date must be a Date instance');
+    });
+
+    it('should not return related error message when params are string dates', async () => {
+      const res = await request(app).post('/rental').send({
+        car_id: '409b03e3-e6ee-4a84-96ea-1bb7cc329091',
+        client_id: '2645a491-2b5d-480f-887d-7ac647903b99',
+        start_date: '2021-10-04',
+        end_date: '2021-10-08',
+      });
+
+      expect(res.body.message).not.toContain(
+        'start_date must be a Date instance',
+      );
+      expect(res.body.message).not.toContain(
+        'end_date must be a Date instance',
+      );
+    });
+  });
 });
