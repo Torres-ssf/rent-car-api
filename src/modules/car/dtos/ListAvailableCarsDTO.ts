1a0d14d40b54303fb2152bcc2babce0d750fa5d9
Torres-ssf <sergio.uft@gmail.com>
2021-02-15 21:34:30 -0300

    feat(car): implement ListAvailableCarsDTO

---
+++ src/modules/car/dtos/ListAvailableCarsDTO.ts
@@ -0,0 +1,47 @@
+import {
+  IsDate,
+  IsEnum,
+  IsNumberString,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+import { Engine, Transmission } from '@modules/car/models/enums';
+import { Transform } from 'class-transformer';
+import { parseISO } from 'date-fns';
+
+export class ListAvailableCarsDTO {
+  @Transform(({ value }) => parseISO(value))
+  @IsDate()
+  start_date: Date;
+
+  @Transform(({ value }) => parseISO(value))
+  @IsDate()
+  end_date: Date;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(80)
+  @MinLength(2)
+  model?: string | undefined;
+
+  @IsOptional()
+  @IsString()
+  @IsEnum(Engine, { message: `engine must be one of ${Object.keys(Engine)}` })
+  @Transform(({ value }) => value.toUpperCase())
+  engine?: Engine | undefined;
+
+  @IsOptional()
+  @IsString()
+  @IsEnum(Transmission, {
+    message: `engine must be one of ${Object.keys(Transmission)}`,
+  })
+  @Transform(({ value }) => value.toUpperCase())
+  transmission?: Transmission | undefined;
+
+  @IsOptional()
+  @IsNumberString()
+  daily_value?: number | undefined;
+}
