0bd79b23a861390a20f6046e0d049b6995495052
Torres-ssf <sergio.uft@gmail.com>
2021-02-15 21:35:30 -0300

    feat(car): add listAvailableCars to the ICarRepository interface

--- src/modules/car/repositories/ICarRepository.ts
+++ src/modules/car/repositories/ICarRepository.ts
@@ -1,8 +1,10 @@
+import { ListAvailableCarsDTO } from '../dtos/ListAvailableCarsDTO';
 import { Car } from '../models/Car';
 
 export interface ICarRepository {
   findById(id: string): Promise<Car | undefined>;
   list(): Promise<Car[]>;
+  listAvailableCars(data: ListAvailableCarsDTO): Promise<Car[]>;
   save(car: Car): Promise<Car>;
   remove(id: string): Promise<void>;
 }
