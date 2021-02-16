fab0310bfee2772380a468cee87882c46d1dc552
Torres-ssf <sergio.uft@gmail.com>
2021-02-15 21:47:01 -0300

    test(list availability car): make sure end date does not come before start date

--- src/modules/car/useCases/ListAvailableCars/ListAvailableCarsUseCase.spec.ts
+++ src/modules/car/useCases/ListAvailableCars/ListAvailableCarsUseCase.spec.ts
@@ -22,4 +22,18 @@ describe('ListCarUseCase', () => {
       }),
     ).rejects.toHaveProperty('message', "start date can't be a past date");
   });
+
+  it('should verify if end_date does not come before start_date', async () => {
+    global.Date.now = jest.fn(() => new Date(2021, 1, 10).getTime());
+
+    await expect(
+      listAvailableCarsUseCase.execute({
+        start_date: new Date(2021, 2, 10),
+        end_date: new Date(2021, 2, 9),
+      }),
+    ).rejects.toHaveProperty(
+      'message',
+      "the end date can't be before the start date",
+    );
+  });
 });
