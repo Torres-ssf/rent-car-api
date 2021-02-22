afc3dc5edbfb931d957d761aca378c959efc6056
Torres-ssf <sergio.uft@gmail.com>
2021-02-22 20:20:04 -0300

    test(list available cars): ensure array with available cars is return

--- src/modules/car/useCases/ListAvailableCars/ListAvailableCarsUseCase.spec.ts
+++ src/modules/car/useCases/ListAvailableCars/ListAvailableCarsUseCase.spec.ts
@@ -1,7 +1,10 @@
 import 'reflect-metadata';
 
 import { FakeCarRepository } from '@modules/car/repositories/fakes/FakeCarRepository';
+import { Engine, Transmission } from '@modules/car/models/enums';
+import { Car } from '@modules/car/models/Car';
 import { ListAvailableCarsUseCase } from './ListAvailableCarsUseCase';
+import carSeed from '../../seeds/cars.json';
 
 describe('ListCarUseCase', () => {
   let listAvailableCarsUseCase: ListAvailableCarsUseCase;
@@ -49,5 +52,30 @@ describe('ListCarUseCase', () => {
       'message',
       "start date and end date can't be at the same day",
     );
+  });
+
+  it('should return an array of cars', async () => {
+    global.Date.now = jest.fn(() => new Date(2021, 1, 10).getTime());
+
+    const newCar = new Car();
+
+    Object.assign(newCar, {
+      ...carSeed[0],
+      engine: carSeed[0].engine as Engine,
+      transmission: carSeed[0].transmission as Transmission,
+      created_at: new Date(carSeed[0].created_at),
+      updated_at: new Date(carSeed[0].updated_at),
+    });
+
+    await carRepository.save(newCar);
+
+    const res = await listAvailableCarsUseCase.execute({
+      start_date: new Date(2021, 2, 10),
+      end_date: new Date(2021, 2, 12),
+    });
+
+    expect(res instanceof Array).toBeTruthy();
+    expect(res.length).toBe(1);
+    expect(res[0] instanceof Car).toBeTruthy();
   });
 });
