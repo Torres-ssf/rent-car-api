199a5b83b8cff6834cbe4f9f92d15e391ed3227c
Torres-ssf <sergio.uft@gmail.com>
2021-03-11 23:20:19 -0300

    feat(user typeorm): implement TypeormUser model

---
+++ src/modules/user/models/typeorm/TypeormUser.ts
@@ -0,0 +1,35 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from '../User';
+
+@Entity('user')
+export class TypeormUser extends User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column({ nullable: true })
+  image: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  admin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
