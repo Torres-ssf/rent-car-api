b2a6842cf17e736031813150358ad88935fd766e
Torres-ssf <sergio.uft@gmail.com>
2021-03-28 18:00:00 -0300

    refactor(user): verify app env before create singleton for IUserRepository

--- src/modules/user/index.ts
+++ src/modules/user/index.ts
@@ -1,10 +1,12 @@
 import './providers';
+import { appEnv } from '@config/environment';
 
 import { container } from 'tsyringe';
 import { IUserRepository } from './repositories/IUserRepository';
-import { TypeormUserRepository } from './repositories/typeorm/TypeormUserRepository';
+import { TypeormUserRepository } from './repositories/implementations/TypeormUserRepository';
+import { FakeUserRepository } from './repositories/fakes/FakeUserRepository';
 
 container.registerSingleton<IUserRepository>(
   'UserRepository',
-  TypeormUserRepository,
+  appEnv === 'test' ? FakeUserRepository : TypeormUserRepository,
 );
