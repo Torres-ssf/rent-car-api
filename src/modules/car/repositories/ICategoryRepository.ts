4df76983fe2700770a4f498b50e90660a824504b
Torres-ssf <sergio.uft@gmail.com>
2021-03-29 13:18:45 -0300

    feat(category): add list method to ICategoryRepository protocol

--- src/modules/car/repositories/ICategoryRepository.ts
+++ src/modules/car/repositories/ICategoryRepository.ts
@@ -4,4 +4,5 @@ import { Category } from '../models/Category';
 export interface ICategoryRepository {
   create({ name, description }: CreateCategoryDTO): Promise<Category>;
   findByName(name: string): Promise<Category | undefined>;
+  listCategories(): Promise<Category[]>;
 }
