9c4f0cde6b80202430d175325bcc0b80019a27bb
Torres-ssf <sergio.uft@gmail.com>
2021-03-29 13:28:49 -0300

    feat(category): add route to list all categories at category routes file

--- src/modules/car/routes/category.routes.ts
+++ src/modules/car/routes/category.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import { CreateCategoryController } from '../useCases/createCategory/CreateCategoryController';
+import { ListCategoriesController } from '../useCases/listCategories/ListCategoriesController';
 
 const createCategoryController = new CreateCategoryController();
+const listCategoriesController = new ListCategoriesController();
 
 export const categoryRoutes = Router();
+
+categoryRoutes.get('/', listCategoriesController.handle);
 
 categoryRoutes.post('/', createCategoryController.handle);
