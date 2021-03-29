0e05778abae8a2f8af892a057f041741f3d2096d
Torres-ssf <sergio.uft@gmail.com>
2021-03-29 12:16:49 -0300

    feat(utils): implement function to validate data from any type of DTO

---
+++ src/shared/utils/dataValidation.ts
@@ -0,0 +1,21 @@
+import { AppError } from '@shared/errors/AppError';
+import { extractValidationErrors } from '@shared/utils/formatValidationError';
+import { plainToClass, ClassConstructor } from 'class-transformer';
+import { validate } from 'class-validator';
+
+export const dataValidation = async <T extends Record<string, unknown>>(
+  DTO: ClassConstructor<T>,
+  dataDTO: T,
+): Promise<T> => {
+  const data = plainToClass(DTO, dataDTO);
+
+  const paramErrors = await validate(data);
+
+  if (paramErrors.length) {
+    const errorMessages = extractValidationErrors(paramErrors);
+
+    throw new AppError(errorMessages);
+  }
+
+  return data;
+};
