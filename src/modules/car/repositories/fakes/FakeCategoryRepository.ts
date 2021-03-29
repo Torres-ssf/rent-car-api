79fa405801f2518e7deb1dbbfe156002374749c0
Torres-ssf <sergio.uft@gmail.com>
2021-03-29 12:14:48 -0300

    feat(category): implement FakeCategoryRepository

---
+++ src/modules/car/repositories/fakes/FakeCategoryRepository.ts
@@ -0,0 +1,21 @@
+import { CreateCategoryDTO } from '@modules/car/dtos/CreateCategoryDTO';
+import { Category } from '@modules/car/models/Category';
+import { ICategoryRepository } from '../ICategoryRepository';
+
+export class FakeCategoryRepository implements ICategoryRepository {
+  private readonly categories: Category[] = [];
+
+  async create({ name, description }: CreateCategoryDTO): Promise<Category> {
+    const newCategory = new Category();
+
+    Object.assign(newCategory, { name, description, created_at: new Date() });
+
+    this.categories.push(newCategory);
+
+    return newCategory;
+  }
+
+  async findByName(name: string): Promise<Category | undefined> {
+    return this.categories.find(eachCategory => eachCategory.name === name);
+  }
+}
