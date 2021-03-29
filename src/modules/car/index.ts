236bfc66a086f666623cc04145adb5d717e144dd
Torres-ssf <sergio.uft@gmail.com>
2021-03-29 15:16:54 -0300

    feat(specification): create SpecificationRepository singleton

--- src/modules/car/index.ts
+++ src/modules/car/index.ts
@@ -5,9 +5,17 @@ import { FakeCarRepository } from './repositories/fakes/FakeCarRepository';
 import { ICategoryRepository } from './repositories/ICategoryRepository';
 import { FakeCategoryRepository } from './repositories/fakes/FakeCategoryRepository';
 
+import { ISpecificationRepository } from './repositories/ISpecificationRepository';
+import { FakeSpecificationRepository } from './repositories/fakes/FakeSpecificationRepository';
+
 container.registerSingleton<ICarRepository>('CarRepository', FakeCarRepository);
 
 container.registerSingleton<ICategoryRepository>(
   'CategoryRepository',
   FakeCategoryRepository,
+);
+
+container.registerSingleton<ISpecificationRepository>(
+  'SpecificationRepository',
+  FakeSpecificationRepository,
 );
