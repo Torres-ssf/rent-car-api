9b2808016be485cbd4453471adfe887f5f99cf40
Torres-ssf <sergio.uft@gmail.com>
2021-03-29 12:14:06 -0300

    feat(category): define ICategoryRepository protocol

---
+++ src/modules/car/repositories/ICategoryRepository.ts
@@ -0,0 +1,7 @@
+import { CreateCategoryDTO } from '../dtos/CreateCategoryDTO';
+import { Category } from '../models/Category';
+
+export interface ICategoryRepository {
+  create({ name, description }: CreateCategoryDTO): Promise<Category>;
+  findByName(name: string): Promise<Category | undefined>;
+}
