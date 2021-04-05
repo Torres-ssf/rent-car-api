02dde5abca2f9ee5a53a5efb997283c582d2a622
Torres-ssf <sergio.uft@gmail.com>
2021-04-05 20:36:46 -0300

    refactor(car): update all car related DTOs

--- src/modules/car/dtos/CarDTO.ts
+++ src/modules/car/dtos/CarDTO.ts
@@ -1,15 +1,12 @@
 import {
-  IsEnum,
   IsNumber,
   IsPositive,
   IsString,
-  Max,
+  IsUUID,
   MaxLength,
-  Min,
   MinLength,
 } from 'class-validator';
 import { Transform } from 'class-transformer';
-import { Engine, Transmission } from '../enums';
 
 export abstract class CarDTO {
   @IsString()
@@ -36,23 +33,17 @@ export abstract class CarDTO {
   @IsNumber()
   abstract zero_to_one_hundred?: number;
 
-  @IsString()
-  @IsEnum(Engine, { message: `engine must be one of ${Object.keys(Engine)}` })
-  @Transform(({ value }) => value.trim().toUpperCase())
-  abstract engine?: Engine;
-
-  @IsString()
-  @IsEnum(Transmission, {
-    message: `engine must be one of ${Object.keys(Transmission)}`,
-  })
-  @Transform(({ value }) => value.trim().toUpperCase())
-  abstract transmission?: Transmission;
-
-  @Min(2)
-  @Max(9)
-  abstract passengers?: number;
-
   @IsPositive()
   @IsNumber()
   abstract daily_value?: number;
+
+  @IsString()
+  abstract license_plate?: string;
+
+  @IsPositive()
+  @IsNumber()
+  abstract fine_amount?: number;
+
+  @IsUUID()
+  abstract category_id?: string;
 }
