75f2440e873234c34757384a4510a76837665672
Torres-ssf <sergio.uft@gmail.com>
2021-04-05 21:25:07 -0300

    test(register car): ensure car cannot be create with existent license plate

--- src/modules/car/useCases/registerCar/RegisterCarUseCase.spec.ts
+++ src/modules/car/useCases/registerCar/RegisterCarUseCase.spec.ts
@@ -1,14 +1,18 @@
-import { Car } from '@modules/car/models/Car';
-import { Engine, Transmission } from '@modules/car/enums';
 import 'reflect-metadata';
 
+import { FakeCategoryRepository } from '@modules/car/repositories/fakes/FakeCategoryRepository';
+import { Category } from '@modules/car/models/Category';
 import { FakeCarRepository } from '../../repositories/fakes/FakeCarRepository';
 import { RegisterCarUseCase } from './RegisterCarUseCase';
 
 describe('RegisterCarUseCase', () => {
-  let registerCarUseCase: RegisterCarUseCase;
+  const carRepository = new FakeCarRepository();
 
-  let carRepository: FakeCarRepository;
+  const categoryRepository = new FakeCategoryRepository();
+
+  const registerCarUseCase = new RegisterCarUseCase(carRepository);
+
+  let dummyCategory: Category;
 
   const carParams = {
     model: 'F8',
@@ -16,41 +20,25 @@ describe('RegisterCarUseCase', () => {
     max_speed: 340,
     horse_power: 720,
     zero_to_one_hundred: 2.9,
-    engine: Engine.Gas,
-    transmission: Transmission.Automatic,
-    passengers: 2,
+    license_plate: 'MWS-2123',
     daily_value: 900,
+    fine_amount: 200,
+    category_id: 'string',
   };
 
-  beforeEach(() => {
-    carRepository = new FakeCarRepository();
-
-    registerCarUseCase = new RegisterCarUseCase(carRepository);
+  beforeAll(async () => {
+    dummyCategory = await categoryRepository.create({
+      name: 'Dummy',
+      description: 'This is a dummy category',
+    });
   });
 
-  it('ensures that all cars properties were properly assigned to the new car object', async () => {
-    const res = await registerCarUseCase.execute(carParams);
+  it('should not register a car with an existent license plate', async () => {
+    await registerCarUseCase.execute(carParams);
 
-    expect(res).toHaveProperty('id');
-    expect(res.id.length).toBeTruthy();
-    expect(res).toHaveProperty('model', 'F8');
-    expect(res).toHaveProperty('brand', 'Ferrari');
-    expect(res).toHaveProperty('max_speed', 340);
-    expect(res).toHaveProperty('horse_power', 720);
-    expect(res).toHaveProperty('zero_to_one_hundred', 2.9);
-    expect(res).toHaveProperty('engine', 'GAS');
-    expect(res).toHaveProperty('transmission', 'AUTOMATIC');
-    expect(res).toHaveProperty('passengers', 2);
-    expect(res).toHaveProperty('daily_value', 900);
-    expect(res).toHaveProperty('created_at');
-    expect(res).toHaveProperty('updated_at');
-  });
-
-  it('ensure new registered car is saved into the database', async () => {
-    const res = await registerCarUseCase.execute(carParams);
-
-    const carInDabase = (await carRepository.findById(res.id)) as Car;
-
-    expect(res).toMatchObject(carInDabase);
+    await expect(registerCarUseCase.execute(carParams)).rejects.toHaveProperty(
+      'message',
+      'License plate already in use',
+    );
   });
 });
