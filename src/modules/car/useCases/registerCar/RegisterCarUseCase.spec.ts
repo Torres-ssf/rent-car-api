99491765c73f247cb78464a3bd5b8b387c1dd28a
Torres-ssf <sergio.uft@gmail.com>
2021-04-06 10:56:49 -0300

    test(register car): ensure car cannot be create without a category

--- src/modules/car/useCases/registerCar/RegisterCarUseCase.spec.ts
+++ src/modules/car/useCases/registerCar/RegisterCarUseCase.spec.ts
@@ -1,18 +1,14 @@
 import 'reflect-metadata';
 
 import { FakeCategoryRepository } from '@modules/car/repositories/fakes/FakeCategoryRepository';
-import { Category } from '@modules/car/models/Category';
+import { v4 } from 'uuid';
 import { FakeCarRepository } from '../../repositories/fakes/FakeCarRepository';
 import { RegisterCarUseCase } from './RegisterCarUseCase';
 
 describe('RegisterCarUseCase', () => {
-  const carRepository = new FakeCarRepository();
-
-  const categoryRepository = new FakeCategoryRepository();
-
-  const registerCarUseCase = new RegisterCarUseCase(carRepository);
-
-  let dummyCategory: Category;
+  let carRepository: FakeCarRepository;
+  let categoryRepository: FakeCategoryRepository;
+  let registerCarUseCase: RegisterCarUseCase;
 
   const carParams = {
     model: 'F8',
@@ -23,22 +19,45 @@ describe('RegisterCarUseCase', () => {
     license_plate: 'MWS-2123',
     daily_value: 900,
     fine_amount: 200,
-    category_id: 'string',
   };
 
-  beforeAll(async () => {
-    dummyCategory = await categoryRepository.create({
-      name: 'Dummy',
-      description: 'This is a dummy category',
-    });
+  beforeAll(() => {
+    carRepository = new FakeCarRepository();
+
+    categoryRepository = new FakeCategoryRepository();
+
+    registerCarUseCase = new RegisterCarUseCase(
+      categoryRepository,
+      carRepository,
+    );
   });
 
   it('should not register a car with an existent license plate', async () => {
-    await registerCarUseCase.execute(carParams);
+    const newCategory = await categoryRepository.create({
+      name: 'Dummy',
+      description: 'This is a dummy category',
+    });
 
-    await expect(registerCarUseCase.execute(carParams)).rejects.toHaveProperty(
-      'message',
-      'License plate already in use',
-    );
+    await registerCarUseCase.execute({
+      ...carParams,
+      category_id: newCategory.id,
+    });
+
+    await expect(
+      registerCarUseCase.execute({
+        ...carParams,
+        category_id: newCategory.id,
+      }),
+    ).rejects.toHaveProperty('message', 'License plate already in use');
+  });
+
+  it('should not be possible to register a new car that belongs to a nonexistent category', async () => {
+    await expect(
+      registerCarUseCase.execute({
+        ...carParams,
+        license_plate: '123123-asda',
+        category_id: v4(),
+      }),
+    ).rejects.toHaveProperty('message', 'Category does not exists');
   });
 });
