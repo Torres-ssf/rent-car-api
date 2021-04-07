f4ec19622200e07c4ab6872cc3d8ce5c8756e92a
Torres-ssf <sergio.uft@gmail.com>
2021-04-07 08:59:38 -0300

    feat(list available cars): ensure category exists when category id is provided

--- src/modules/car/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
+++ src/modules/car/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
@@ -1,15 +1,32 @@
 import { inject, injectable } from 'tsyringe';
 import { Car } from '@modules/car/models/Car';
 import { ICarRepository } from '@modules/car/repositories/ICarRepository';
+import { ListAvailableCarsDTO } from '@modules/car/dtos/ListAvailableCarsDTO';
+import { ICategoryRepository } from '@modules/car/repositories/ICategoryRepository';
+import { AppError } from '@shared/errors/AppError';
 
 @injectable()
 export class ListAvailableCarsUseCase {
   constructor(
     @inject('CarRepository')
     private carRepository: ICarRepository,
+    @inject('CategoryRepository')
+    private categoryRepository: ICategoryRepository,
   ) {}
 
-  async execute(): Promise<Car[]> {
+  async execute(listAvailableCarsDTO: ListAvailableCarsDTO): Promise<Car[]> {
+    const { category_id } = listAvailableCarsDTO;
+
+    if (category_id) {
+      const categoryExists = await this.categoryRepository.findById(
+        category_id,
+      );
+
+      if (!categoryExists) {
+        throw new AppError('Category does not exists');
+      }
+    }
+
     return this.carRepository.listAvailableCars();
   }
 }
