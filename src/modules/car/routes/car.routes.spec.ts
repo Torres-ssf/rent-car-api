2a5d9ad8780855631730d00f130a1932de727dca
Torres-ssf <sergio.uft@gmail.com>
2021-04-07 13:26:36 -0300

    test(car): add an admin user to car routes tests

--- src/modules/car/routes/car.routes.spec.ts
+++ src/modules/car/routes/car.routes.spec.ts
@@ -2,6 +2,10 @@ import 'reflect-metadata';
 
 import request from 'supertest';
 import { app } from '@shared/app';
+import { RegisterUserUseCase } from '@modules/user/useCases/registerUser/RegisterUserUseCase';
+import { CreateSessionUseCase } from '@modules/user/useCases/createSession/CreateSessionUseCase';
+import { container } from 'tsyringe';
+import { IUserRepository } from '@modules/user/repositories/IUserRepository';
 
 describe('Car Register Endpoint', () => {
   const carParams = {
@@ -15,20 +19,52 @@ describe('Car Register Endpoint', () => {
     fine_amount: 200,
   };
 
-  it('ensures that model and brand have at least 2 chars length and max 80 chars length', async () => {
-    const resMinLength = await request(app).post('/car').send({
-      model: 'a',
-      brand: 's',
-      daily_value: 900,
+  let jwtToken = '';
+
+  beforeAll(async () => {
+    const userRepository = container.resolve<IUserRepository>('UserRepository');
+
+    const registerUser = container.resolve(RegisterUserUseCase);
+
+    const createSession = container.resolve(CreateSessionUseCase);
+
+    const newUser = await registerUser.execute({
+      name: 'Admin',
+      email: 'admin@email.com',
+      password: 'aA123456',
+      driver_license: '234523452345',
     });
 
-    const resMaxLength = await request(app).post('/car').send({
-      model:
-        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially un',
-      brand:
-        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially un',
-      daily_value: 900,
+    await userRepository.save({ ...newUser, admin: true });
+
+    const res = await createSession.execute({
+      email: newUser.email,
+      password: newUser.password,
     });
+
+    jwtToken = res.token;
+  });
+
+  it('should ensure that model and brand have at least 2 chars length and max 80 chars length', async () => {
+    const resMinLength = await request(app)
+      .post('/car')
+      .set('Authorization', `Bearer ${jwtToken}`)
+      .send({
+        model: 'a',
+        brand: 's',
+        daily_value: 900,
+      });
+
+    const resMaxLength = await request(app)
+      .post('/car')
+      .set('Authorization', `Bearer ${jwtToken}`)
+      .send({
+        model:
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially un',
+        brand:
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially un',
+        daily_value: 900,
+      });
 
     expect(resMinLength.status).toBe(400);
     expect(resMinLength.body.message).toContain(
@@ -47,13 +83,16 @@ describe('Car Register Endpoint', () => {
     );
   });
 
-  it('ensures that max_speed, horse_power, zero_to_one_hundred, daily_value are positive values', async () => {
-    const res = await request(app).post('/car').send({
-      max_speed: -380,
-      horse_power: -899,
-      zero_to_one_hundred: -2.4,
-      daily_value: -900,
-    });
+  it('should ensures that max_speed, horse_power, zero_to_one_hundred, daily_value are positive values', async () => {
+    const res = await request(app)
+      .post('/car')
+      .set('Authorization', `Bearer ${jwtToken}`)
+      .send({
+        max_speed: -380,
+        horse_power: -899,
+        zero_to_one_hundred: -2.4,
+        daily_value: -900,
+      });
 
     expect(res.status).toBe(400);
     expect(res.body.message).toContain('max_speed must be a positive number');
@@ -65,13 +104,17 @@ describe('Car Register Endpoint', () => {
   });
 
   it('ensures all Car string properties have no empty spaces at the start/end', async () => {
-    const resCat = await request(app).post('/category').send({
-      name: 'SUV',
-      description: 'Big car for the family',
-    });
+    const resCat = await request(app)
+      .post('/category')
+      .set('Authorization', `Bearer ${jwtToken}`)
+      .send({
+        name: 'SUV',
+        description: 'Big car for the family',
+      });
 
     const res = await request(app)
       .post('/car')
+      .set('Authorization', `Bearer ${jwtToken}`)
       .send({
         ...carParams,
         model: '          Enzo ',
