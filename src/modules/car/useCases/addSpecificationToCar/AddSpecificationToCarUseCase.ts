f4a9dbd56931a0fbe48cd790d34ef0b015b54024
Torres-ssf <sergio.uft@gmail.com>
2021-04-08 14:25:35 -0300

    feat(add specification car): ensure car exists before add specification to it

---
+++ src/modules/car/useCases/addSpecificationToCar/AddSpecificationToCarUseCase.ts
@@ -0,0 +1,30 @@
+import { AddSpecificationToCarDTO } from '@modules/car/dtos/AddSpecificationToCarDTO';
+import { Car } from '@modules/car/models/Car';
+import { ICarRepository } from '@modules/car/repositories/ICarRepository';
+import { ISpecificationRepository } from '@modules/specification/repositories/ISpecificationRepository';
+import { AppError } from '@shared/errors/AppError';
+import { inject, injectable } from 'tsyringe';
+
+@injectable()
+export class AddSpecificationToCarUseCase {
+  constructor(
+    @inject('CarRepository')
+    private carRepository: ICarRepository,
+    @inject('SpecificationRepository')
+    private specificationRepository: ISpecificationRepository,
+  ) {}
+
+  async execute(
+    addSpecificationToCarDTO: AddSpecificationToCarDTO,
+  ): Promise<Car> {
+    const { car_id, specifications_ids } = addSpecificationToCarDTO;
+
+    const carExists = await this.carRepository.findById(car_id);
+
+    if (!carExists) {
+      throw new AppError('No car found for the given car id');
+    }
+
+    return new Car();
+  }
+}
