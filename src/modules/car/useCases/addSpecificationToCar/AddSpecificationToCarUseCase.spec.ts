6064737845bb6c8c3a19cd1e7d6d5a23e6f0178e
Torres-ssf <sergio.uft@gmail.com>
2021-04-08 14:41:04 -0300

    test(add specification car): ensure at least one specification was found for the given ids

--- src/modules/car/useCases/addSpecificationToCar/AddSpecificationToCarUseCase.spec.ts
+++ src/modules/car/useCases/addSpecificationToCar/AddSpecificationToCarUseCase.spec.ts
@@ -4,6 +4,7 @@ import { Car } from '@modules/car/models/Car';
 import { FakeCarRepository } from '@modules/car/repositories/fakes/FakeCarRepository';
 import { FakeSpecificationRepository } from '@modules/specification/repositories/fakes/FakeSpecificationRepository';
 import { v4 } from 'uuid';
+import { FakeCategoryRepository } from '@modules/car/repositories/fakes/FakeCategoryRepository';
 import { AddSpecificationToCarUseCase } from './AddSpecificationToCarUseCase';
 
 describe('AddSpecificationToCar', () => {
@@ -11,12 +12,27 @@ describe('AddSpecificationToCar', () => {
 
   let specificationRepository: FakeSpecificationRepository;
 
+  let categoryRepository: FakeCategoryRepository;
+
   let addSpecificationToCarUseCase: AddSpecificationToCarUseCase;
+
+  const carParams = {
+    model: 'F8',
+    brand: 'Ferrari',
+    max_speed: 340,
+    horse_power: 720,
+    zero_to_one_hundred: 2.9,
+    daily_value: 800,
+    fine_amount: 200,
+    license_plate: '1234-MSH',
+  };
 
   beforeAll(() => {
     carRepository = new FakeCarRepository();
 
     specificationRepository = new FakeSpecificationRepository();
+
+    categoryRepository = new FakeCategoryRepository();
 
     addSpecificationToCarUseCase = new AddSpecificationToCarUseCase(
       carRepository,
@@ -31,5 +47,27 @@ describe('AddSpecificationToCar', () => {
         specifications_ids: [v4()],
       }),
     ).rejects.toHaveProperty('message', 'No car found for the given car id');
+  });
+
+  it('should found at least one specification for the given specification ids', async () => {
+    const suvCategory = await categoryRepository.create({
+      name: 'SUV',
+      description: 'Sport Utility Vehicle',
+    });
+
+    const newCar = await carRepository.create({
+      ...carParams,
+      category_id: suvCategory.id,
+    });
+
+    await expect(
+      addSpecificationToCarUseCase.execute({
+        car_id: newCar.id,
+        specifications_ids: [v4()],
+      }),
+    ).rejects.toHaveProperty(
+      'message',
+      'No specifications were found for the given ids',
+    );
   });
 });
