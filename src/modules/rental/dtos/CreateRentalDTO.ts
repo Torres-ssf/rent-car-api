20e2598a76b40530115c4b7e6fd5f3182fae649d
Torres-ssf <sergio.uft@gmail.com>
2021-04-10 14:17:00 -0300

    refactor(rental): remove decorators from CreateRentalDTO

--- src/modules/rental/dtos/CreateRentalDTO.ts
+++ src/modules/rental/dtos/CreateRentalDTO.ts
@@ -1,28 +1,15 @@
-import { Transform } from 'class-transformer';
-import { IsDate, IsUUID, IsEmpty } from 'class-validator';
-import { parseISO } from 'date-fns';
-
 export class CreateRentalDTO {
-  @IsUUID('4')
   car_id: string;
 
-  @IsUUID('4')
   user_id: string;
 
-  @Transform(({ value }) => parseISO(value))
-  @IsDate()
   start_date: Date;
 
-  @Transform(({ value }) => parseISO(value))
-  @IsDate()
   expected_return_date: Date;
 
-  @IsEmpty()
   car_daily_value: number;
 
-  @IsEmpty()
   car_daily_fine: number;
 
-  @IsEmpty()
   estimated_total: number;
 }
