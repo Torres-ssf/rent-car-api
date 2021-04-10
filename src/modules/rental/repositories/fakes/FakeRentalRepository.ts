7118441ee80b8b248c52441b839328441dd570a7
Torres-ssf <sergio.uft@gmail.com>
2021-04-10 10:22:22 -0300

    refactor(rental): refactor FakeRentalRepository

--- src/modules/rental/repositories/fakes/FakeRentalRepository.ts
+++ src/modules/rental/repositories/fakes/FakeRentalRepository.ts
@@ -1,28 +1,47 @@
-import { IFindByCarAndDate } from '@modules/rental/dtos/IFindByCarAndDate';
+import { CreateRentalDTO } from '@modules/rental/dtos/CreateRentalDTO';
+import { Status } from '@modules/rental/enums/Status';
 import { Rental } from '@modules/rental/models/Rental';
 import { IRentalRepository } from '../IRentalRepository';
 
 export class FakeRentalRepository implements IRentalRepository {
-  rentals: Rental[] = [];
+  private readonly rentals: Rental[] = [];
+
+  async create(createRentalDTO: CreateRentalDTO): Promise<Rental> {
+    const newRental = new Rental();
+
+    Object.assign(newRental, {
+      ...createRentalDTO,
+      status: Status.Open,
+    });
+
+    this.rentals.push(newRental);
+
+    return newRental;
+  }
+
+  async findOpenRentalForCar(carId: string): Promise<Rental | undefined> {
+    return this.rentals.find(
+      rental => rental.car_id === carId && rental.status === Status.Open,
+    );
+  }
+
+  async findOpenRentalForUser(userId: string): Promise<Rental | undefined> {
+    return this.rentals.find(
+      rental => rental.user_id === userId && rental.status === Status.Open,
+    );
+  }
 
   async save(rental: Rental): Promise<Rental> {
-    this.rentals.push(rental);
+    const rentalIndex = this.rentals.findIndex(
+      eachRental => eachRental.id === rental.id,
+    );
+
+    if (rentalIndex > -1) {
+      this.rentals[rentalIndex] = rental;
+    } else {
+      this.rentals.push(rental);
+    }
+
     return rental;
-  }
-
-  async findByCarIdAndDate(data: IFindByCarAndDate): Promise<Rental[]> {
-    const { car_id, start_date, end_date } = data;
-
-    return this.rentals.filter(rental => {
-      return (
-        rental.car_id === car_id &&
-        ((rental.start_date <= start_date && rental.end_date >= start_date) ||
-          (rental.start_date >= start_date && rental.start_date <= end_date))
-      );
-    });
-  }
-
-  findByUserId(userId: string): Promise<Rental> {
-    throw new Error('Method not implemented.');
   }
 }
