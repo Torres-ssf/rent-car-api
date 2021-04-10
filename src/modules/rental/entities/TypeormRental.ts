76d508625296f59ff660219f60938635f351c0a4
Torres-ssf <sergio.uft@gmail.com>
2021-04-10 08:38:10 -0300

    feat(rental): implement TypeormRental entity

---
+++ src/modules/rental/entities/TypeormRental.ts
@@ -0,0 +1,49 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Status } from '../enums/Status';
+import { Rental } from '../models/Rental';
+
+@Entity('rental')
+export class TypeormRental extends Rental {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  car_id: string;
+
+  @Column()
+  user_id: string;
+
+  @Column()
+  start_date: Date;
+
+  @Column()
+  expected_return_date: Date;
+
+  @Column()
+  returned_date: Date | null;
+
+  @Column()
+  status: Status;
+
+  @Column()
+  estimated_total: number;
+
+  @Column()
+  total: number | null;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    super();
+  }
+}
