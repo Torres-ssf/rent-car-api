4647f49fd2b6851c3923abcdac2e355f6db16a2b
Torres-ssf <sergio.uft@gmail.com>
2021-04-11 14:47:41 -0300

    feat(app routes): add categories routes to app routes

--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 import { userRoutes } from '@modules/user/routes/user.routes';
 import { sessionRoutes } from '@modules/user/routes/session.routes';
 import { carRoutes } from '@modules/car/routes/car.routes';
-import { categoryRoutes } from '@modules/car/routes/category.routes';
+import { categoryRoutes } from '@modules/category/routes/category.routes';
 import { specificationRoutes } from '@modules/specification/routes/specification.routes';
 import { rentalRoutes } from '@modules/rental/routes/rental.routes';
 import { carImageRoutes } from '@modules/carImage/routes/carImages.routes';
