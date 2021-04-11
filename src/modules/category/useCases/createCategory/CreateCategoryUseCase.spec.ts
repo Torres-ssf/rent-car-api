7f89597759e05a300667df316a8ad2d7a061ed6a
Torres-ssf <sergio.uft@gmail.com>
2021-04-11 13:36:40 -0300

    test(create category): ensure a category is create with the given parameters

--- src/modules/category/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/category/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -30,4 +30,20 @@ describe('CreateCategoryUseCase', () => {
       `Category with name ${dummyCategory.name} already exists`,
     );
   });
+
+  it('should create category if the given parameters', async () => {
+    const categoryName = 'SUV';
+    const categoryDescription =
+      'Sport utility vehicle or SUV is a car classification that combines elements of road-going passenger cars with features from off-road vehicles.';
+
+    const category = await createCategoryUseCase.execute({
+      name: categoryName,
+      description: categoryDescription,
+    });
+
+    expect(category).toHaveProperty('id');
+    expect(category).toHaveProperty('name', categoryName);
+    expect(category).toHaveProperty('description', categoryDescription);
+    expect(category).toHaveProperty('created_at');
+  });
 });
