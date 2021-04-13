e34aff98e01194f316179d54b18d22e52461483f
Torres-ssf <sergio.uft@gmail.com>
2021-04-13 11:54:31 -0300

    test(create category int): ensure create category endpoint requires an authenticated user

---
+++ src/modules/category/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,60 @@
+import 'reflect-metadata';
+
+import request from 'supertest';
+import { app } from '@shared/app';
+import { Connection, createConnection } from 'typeorm';
+import { User } from '@modules/user/models/User';
+import {
+  getAdminAuthToken,
+  getUserAuthToken,
+} from '@modules/user/seeds/user.seeds';
+import { Category } from '@modules/category/models/Category';
+
+describe('Create Category Endpoint', () => {
+  let connection: Connection;
+
+  let userToken: string;
+
+  let adminToken: string;
+
+  beforeAll(async () => {
+    connection = await createConnection();
+
+    await connection.runMigrations();
+
+    adminToken = await getAdminAuthToken(connection);
+
+    userToken = await getUserAuthToken(connection);
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+
+    await connection.close();
+  });
+
+  it('should verify if user is an authenticated user', async () => {
+    await request(app)
+      .post('/category')
+      .send({
+        name: 'Sport',
+        description: 'Sports cars category',
+      })
+      .expect(401)
+      .expect(res =>
+        expect(res.body).toHaveProperty('message', 'JWT token is missing'),
+      );
+
+    await request(app)
+      .post('/category')
+      .set('Authorization', `Bearer 23452345823490-5890-23485-9023485`)
+      .send({
+        name: 'Sport',
+        description: 'Sports cars category',
+      })
+      .expect(401)
+      .expect(res =>
+        expect(res.body).toHaveProperty('message', 'Invalid JWT token'),
+      );
+  });
+});
