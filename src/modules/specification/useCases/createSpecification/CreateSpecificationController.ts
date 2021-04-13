e8526deac6ff9f6175e63dcda46034cbd0b51c38
Torres-ssf <sergio.uft@gmail.com>
2021-04-13 15:46:35 -0300

    refactor(create speficiation): update status response on the Controller

--- src/modules/specification/useCases/createSpecification/CreateSpecificationController.ts
+++ src/modules/specification/useCases/createSpecification/CreateSpecificationController.ts
@@ -1,5 +1,4 @@
 import { CreateSpecificationDTO } from '@modules/specification/dtos/CreateSpecificationDTO';
-import { AppError } from '@shared/errors/AppError';
 import { dataValidation } from '@shared/utils/dataValidation';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
@@ -7,25 +6,19 @@ import { CreateSpecificationUseCase } from './CreateSpecificationUseCase';
 
 export class CreateSpecificationController {
   async handle(request: Request, response: Response): Promise<Response> {
-    try {
-      const createSpecificationDTO = await dataValidation(
-        CreateSpecificationDTO,
-        request.body,
-      );
+    const createSpecificationDTO = await dataValidation(
+      CreateSpecificationDTO,
+      request.body,
+    );
 
-      const createSpecificationUseCase = container.resolve(
-        CreateSpecificationUseCase,
-      );
+    const createSpecificationUseCase = container.resolve(
+      CreateSpecificationUseCase,
+    );
 
-      const newSpecification = await createSpecificationUseCase.execute(
-        createSpecificationDTO,
-      );
+    const newSpecification = await createSpecificationUseCase.execute(
+      createSpecificationDTO,
+    );
 
-      return response.json(newSpecification);
-    } catch (err) {
-      throw new AppError(
-        err.message || 'Error occurred while creating new specification',
-      );
-    }
+    return response.status(201).json(newSpecification);
   }
 }
