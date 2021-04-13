2a9ab4c1664fa3a504c1eba3ba6be46c5d31e925
Torres-ssf <sergio.uft@gmail.com>
2021-04-13 14:33:06 -0300

    test(list specifications): ensure all specifications are listed

---
+++ src/modules/specification/useCases/listSpecifications/ListSpecificationsUseCase.spec.ts
@@ -0,0 +1,41 @@
+import 'reflect-metadata';
+
+import { randomizeANumber } from '@shared/utils/randomizeANumber';
+import { FakeSpecificationRepository } from '@modules/specification/repositories/fakes/FakeSpecificationRepository';
+import { ListSpecificationsUserCase } from './ListSpecificationsUseCase';
+import specificationsSeeds from '../../seeds/specifications.json';
+
+describe('ListSpecificationsUseCase', () => {
+  let specificationRepository: FakeSpecificationRepository;
+
+  let listSpecificationsUseCase: ListSpecificationsUserCase;
+
+  beforeAll(() => {
+    specificationRepository = new FakeSpecificationRepository();
+
+    listSpecificationsUseCase = new ListSpecificationsUserCase(
+      specificationRepository,
+    );
+  });
+
+  it('should list all created specifications', async () => {
+    const randomNumber = randomizeANumber(1, specificationsSeeds.length);
+
+    const promises = [];
+
+    for (let i = 0; i < randomNumber; i += 1) {
+      promises.push(
+        specificationRepository.create({
+          name: specificationsSeeds[i].name,
+          description: specificationsSeeds[i].description,
+        }),
+      );
+    }
+
+    await Promise.all(promises);
+
+    const specificationsList = await listSpecificationsUseCase.execute();
+
+    expect(specificationsList.length).toBe(randomNumber);
+  });
+});
