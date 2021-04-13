42eb8178663c4faa820bf735ea6c14de9374aa57
Torres-ssf <sergio.uft@gmail.com>
2021-04-13 15:57:01 -0300

    feat(specification): add list endpoint to routes file

--- src/modules/specification/routes/specification.routes.ts
+++ src/modules/specification/routes/specification.routes.ts
@@ -2,10 +2,18 @@ import { ensureAdminMiddleware } from '@shared/middlewares/ensureAdminMiddleware
 import { ensureAuthenticatedMiddleware } from '@shared/middlewares/ensureAuthenticatedMiddleware';
 import { Router } from 'express';
 import { CreateSpecificationController } from '../useCases/createSpecification/CreateSpecificationController';
+import { ListSpecificationsController } from '../useCases/listSpecifications/ListSpecificationsController';
 
+const listSpecificationsController = new ListSpecificationsController();
 const createSpecificationController = new CreateSpecificationController();
 
 export const specificationRoutes = Router();
+
+specificationRoutes.get(
+  '/',
+  ensureAuthenticatedMiddleware,
+  listSpecificationsController.handle,
+);
 
 specificationRoutes.post(
   '/',
