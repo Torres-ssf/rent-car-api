4a03fde107cf64fb3f6b55d3009517280e353b4b
Torres-ssf <sergio.uft@gmail.com>
2021-04-13 11:21:15 -0300

    refactor(create category): remove try/catch block

--- src/modules/category/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/category/useCases/createCategory/CreateCategoryController.ts
@@ -1,5 +1,4 @@
 import { CreateCategoryDTO } from '@modules/category/dtos/CreateCategoryDTO';
-import { AppError } from '@shared/errors/AppError';
 import { dataValidation } from '@shared/utils/dataValidation';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
@@ -7,22 +6,14 @@ import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
 export class CreateCategoryController {
   async handle(request: Request, response: Response): Promise<Response> {
-    try {
-      const createCategoryDTO = await dataValidation(
-        CreateCategoryDTO,
-        request.body,
-      );
-      const createCategoryUseCase = container.resolve(CreateCategoryUseCase);
+    const createCategoryDTO = await dataValidation(
+      CreateCategoryDTO,
+      request.body,
+    );
+    const createCategoryUseCase = container.resolve(CreateCategoryUseCase);
 
-      const newCategory = await createCategoryUseCase.execute(
-        createCategoryDTO,
-      );
+    const newCategory = await createCategoryUseCase.execute(createCategoryDTO);
 
-      return response.json(newCategory);
-    } catch (err) {
-      throw new AppError(
-        err.message || 'Error occurred while trying to create new category.',
-      );
-    }
+    return response.status(201).json(newCategory);
   }
 }
