dbc7d6fbf43264ce8da39e7b6f6910ec5fb585be
Torres-ssf <sergio.uft@gmail.com>
2021-04-14 13:01:03 -0300

    refactor(list available cars): refactor DTO

--- src/modules/car/dtos/ListAvailableCarsDTO.ts
+++ src/modules/car/dtos/ListAvailableCarsDTO.ts
@@ -1,14 +1,16 @@
-import { IsOptional, IsString, IsUUID } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, IsUUID } from 'class-validator';
 import { Transform } from 'class-transformer';
 
 export class ListAvailableCarsDTO {
   @IsOptional()
   @IsString()
+  @IsNotEmpty()
   @Transform(({ value }) => value.trim())
   model?: string;
 
   @IsOptional()
   @IsString()
+  @IsNotEmpty()
   @Transform(({ value }) => value.trim())
   brand?: string;
 
