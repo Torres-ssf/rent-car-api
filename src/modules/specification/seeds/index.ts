18d3345aa3bbf4c30822513399090e30212d192c
Torres-ssf <sergio.uft@gmail.com>
2021-04-17 12:58:21 -0300

    test(specification): implement function to create dummies specifications

--- src/modules/specification/seeds/index.ts
+++ src/modules/specification/seeds/index.ts
@@ -1,13 +1,17 @@
 import { Connection } from 'typeorm';
 import { v4 } from 'uuid';
+import { TypeormSpecification } from '../entities/TypeormSpecification';
 import { Specification } from '../models/Specification';
 
 export const createSpecifications = async (
   connection: Connection,
   specificationsParams: Pick<Specification, 'name' | 'description'>[],
-): Promise<void> => {
+): Promise<string[]> => {
+  const ids: string[] = [];
   const specificationsQuery = specificationsParams.map(params => {
     const id = v4();
+
+    ids.push(id);
 
     return connection.query(
       `INSERT INTO specification(id, name, description)
@@ -16,4 +20,34 @@ export const createSpecifications = async (
   });
 
   await Promise.all(specificationsQuery);
+
+  return ids;
+};
+
+export const createDummySpecifications = async (
+  connection: Connection,
+  numOfSpecifications: number,
+): Promise<string[]> => {
+  const ids: string[] = [];
+
+  await connection
+    .createQueryBuilder()
+    .insert()
+    .into(TypeormSpecification)
+    .values(
+      Array.from({ length: numOfSpecifications }, (_, index) => {
+        const id = v4();
+
+        ids.push(id);
+
+        return {
+          id,
+          name: `Dummy ${index}`,
+          description: `This is a dummy spec ${index}`,
+        };
+      }),
+    )
+    .execute();
+
+  return ids;
 };
