96274c17b84c0195c363b6492d9e2ff3a93de8cf
Torres-ssf <sergio.uft@gmail.com>
2021-04-20 20:41:43 -0300

    refactor(create session): remove try catch block at CreateSessionController

--- src/modules/user/useCases/createSession/CreateSessionController.ts
+++ src/modules/user/useCases/createSession/CreateSessionController.ts
@@ -1,4 +1,3 @@
-import { AppError } from '@shared/errors/AppError';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import { dataValidation } from '@shared/utils/dataValidation';
@@ -7,27 +6,21 @@ import { CreateSessionDTO } from '../../dtos/CreateSessionDTO';
 
 export class CreateSessionController {
   async handle(request: Request, response: Response): Promise<Response> {
-    try {
-      const { email, password } = await dataValidation(
-        CreateSessionDTO,
-        request.body,
-      );
+    const { email, password } = await dataValidation(
+      CreateSessionDTO,
+      request.body,
+    );
 
-      const createSessionUseCase = container.resolve(CreateSessionUseCase);
+    const createSessionUseCase = container.resolve(CreateSessionUseCase);
 
-      const {
-        user: { password: removed, ...user },
-        token,
-      } = await createSessionUseCase.execute({
-        email,
-        password,
-      });
+    const {
+      user: { password: removed, ...user },
+      token,
+    } = await createSessionUseCase.execute({
+      email,
+      password,
+    });
 
-      return response.json({ user, token });
-    } catch (err) {
-      throw new AppError(
-        err.message || 'Error occurred when trying to create new session',
-      );
-    }
+    return response.json({ user, token });
   }
 }
